434dd45bd0177be5a8b672d5a37ebaae130e4e6f
Tyler B <bremont@usf.edu>
2026-03-08 09:51:45 -0400

    Created elements in cc_7.js

---
+++ cc_7.js
@@ -0,0 +1,4 @@
+const headlineInput = document.getElementById("headline-input");
+const updateButton = document.getElementById("update-btn");
+const ctaHeadline = document.getElementById("cta-headline");
+
